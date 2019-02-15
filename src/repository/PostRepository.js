644c6da028d0c82d3818f8fd712f8e9d1f7271a5
Xin Li <aehrt55@aehrt55.net>
2019-02-15 14:59:26 +0800

    add validator

--- src/repository/PostRepository.js
+++ src/repository/PostRepository.js
@@ -1,9 +1,13 @@
 import Sequelize from 'sequelize'
+import isMongoId from 'validator/lib/isMongoId'
 import Post from '../model/Post'
 import PostLike from '../sequelize-models/PostLike'
 
 export default class PostRepository {
   static async findById(id) {
+    if (!isMongoId(id)) {
+      return null
+    }
     return await Post.findById(id)
   }
   static async findTop(limit, skip = 0) {
--- src/repository/UserRepository.js
+++ src/repository/UserRepository.js
@@ -1,7 +1,11 @@
+import isMongoId from 'validator/lib/isMongoId'
 import User from '../model/User'
 
 export default class UserRepository {
   static async findById(id) {
+    if (!isMongoId(id)) {
+      return null
+    }
     return await User.findById(id)
   }
 }
